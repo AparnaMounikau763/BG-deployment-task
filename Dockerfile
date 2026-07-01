4e3fdfe0dc6e0f5cf65782bac21d1071160f6d17
Mounika <aparnamounikaanupoju@gmail.com>
2026-07-01 23:18:26 +0530

    Updated code

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,11 @@ FROM python:3.12-slim
 WORKDIR /app
 
 COPY requirements.txt .
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
 EXPOSE 5000
 
-CMD ["gunicorn","--bind","0.0.0.0:5000","run:app"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,41 +1,20 @@
-import os
-import logging
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
-# Configuration
-APP_NAME = "Flask Blue Green - V2"
-VERSION = os.getenv("APP_VERSION", "v2")
-ENVIRONMENT = os.getenv("FLASK_ENV", "production")
-
-# Logging Configuration
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s"
-)
-
+VERSION = os.getenv("VERSION", "BLUE")
 
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "Flask Blue-Green Deployment - v2",
-        "version": VERSION,
-        "environment": ENVIRONMENT
-    })
-
+    return f"<h1>{VERSION} Environment</h1>"
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "UP"}), 200
-
-
-@app.route("/version")
-def version():
     return jsonify({
+        "status": "UP",
         "version": VERSION
     })
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
