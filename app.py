97869dd1538af818b525eb0a70f2e911b93d12cb
Mounika <aparnamounikaanupoju@gmail.com>
2026-07-02 19:27:46 +0530

    New code

--- app.py
+++ app.py
@@ -1,20 +1,57 @@
 from flask import Flask, jsonify
 import os
+import socket
+from datetime import datetime
 
 app = Flask(__name__)
 
-VERSION = os.getenv("VERSION", "BLUE")
+# Read deployment version from environment variable
+VERSION = os.getenv("VERSION", "blue")
+
+HOSTNAME = socket.gethostname()
+
 
 @app.route("/")
 def home():
-    return f"<h1>{VERSION} Environment</h1>"
+
+    return jsonify({
+        "message": f"Welcome to {VERSION.upper()} Environment",
+        "environment": VERSION,
+        "hostname": HOSTNAME,
+        "status": "Running",
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+
 
 @app.route("/health")
 def health():
+
     return jsonify({
         "status": "UP",
-        "version": VERSION
+        "environment": VERSION
+    }), 200
+
+
+@app.route("/version")
+def version():
+
+    return jsonify({
+        "environment": VERSION
     })
+
+
+@app.route("/info")
+def info():
+
+    return jsonify({
+        "Application": "Blue Green Deployment Demo",
+        "Environment": VERSION,
+        "Hostname": HOSTNAME,
+        "Python": "Flask",
+        "Deployment": VERSION.upper(),
+        "Timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
